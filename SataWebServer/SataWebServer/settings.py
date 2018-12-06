a8f838d3fc798ab070f7018195615463e19511ae
ynxu <ynxu15@gmail.com>
2018-12-06 14:55:41 +0800

    test celery, ajax, and database

--- SataWebServer/SataWebServer/settings.py
+++ SataWebServer/SataWebServer/settings.py
@@ -123,3 +123,14 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+
+# Celery settings
+
+CELERY_BROKER_URL = 'amqp://guest:guest@localhost//'
+
+#: Only add pickle to this list if your broker is secured
+#: from unwanted access (see userguide/security.html)
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_RESULT_BACKEND = 'db+sqlite:///results.sqlite'
+CELERY_TASK_SERIALIZER = 'json'
